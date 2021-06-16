fd7f0d453d73249cd1cbbbe6d63b5653b604c0b8
ricardo.rocker <ricardo.rocker@itix.com.br>
2021-06-15 23:37:14 -0300

    mockPersonagens e ngFor

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Naruto' },
+    { id: 12, name: 'Suseke' },
+    { id: 13, name: 'Sakura' },
+    { id: 14, name: 'Hinata' },
+    { id: 15, name: 'Sai' },
+    { id: 16, name: 'Kakashi' },
+    { id: 17, name: 'Jiraya' },
+    { id: 18, name: 'Tsunade' },
+    { id: 19, name: 'Neji' },
+    { id: 20, name: 'Gaara' }
+];
